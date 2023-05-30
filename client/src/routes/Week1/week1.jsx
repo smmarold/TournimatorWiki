284da3efdb7ee0668d0a777879b3e48a47dce082
smmarold <stephen.marold@gmail.com>
2023-05-30 18:30:46 -0400

    more fix 2

--- client/src/routes/Week1/week1.jsx
+++ client/src/routes/Week1/week1.jsx
@@ -7,10 +7,10 @@ import '../styles.css';
 // import { PDFDocumentProxy } from 'pdfjs-dist';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-	'pdfjs-dist/build/pdf.worker.min.js',
-	import.meta.url
-).toString();
+// pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+// 	'pdfjs-dist/build/pdf.worker.min.js',
+// 	import.meta.url
+// ).toString();
 
 const options = {
 	cMapUrl: 'cmaps/',
